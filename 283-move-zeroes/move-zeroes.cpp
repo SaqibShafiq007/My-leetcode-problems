c2debc1a954e79b056938c6e078e8e30178e1536
Saqib Shafiq <saqibshafiq618@gmail.com>
2026-06-09 10:24:54 +0500

    Time: 0 ms (100.00%) | Memory: 23.9 MB (54.53%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-
-
         int n = nums.size();
 
+        //find 1st 0 element
+        int zero = 0;
+        while( zero<n  && nums[zero]!=0      ){
+            zero++;
+        }
         
+        if(zero==n) return;
 
-       
 
-        //finf 1st zero
-        int i=0;
-        while(i < n) {
-             if(nums[i] == 0 ) break;
-             i++;
+        //find 1st non zero  element after 1st zero
+        int non_zero = zero;
+        while(non_zero<n && nums[non_zero]==0 ){
+            non_zero++;
         }
 
-        //if all elements in array are 0
-        if(i==n ) return;
-
-       
-
-        
-        for(int j = i+1 ; j < n ; j++){
-            if(nums[j]!=0){
-                swap(nums[i] , nums[j]);
-                i++;                
+        while (zero<n && non_zero<n){
+            if(nums[non_zero]!=0){
+                swap(nums[zero],nums[non_zero]);
+                zero++;
             }
+            non_zero++;
         }
-
-        
     }
+
+
+
+       
 };
