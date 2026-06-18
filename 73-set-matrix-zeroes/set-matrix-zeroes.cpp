a2f8f66edd5fd0caaa2fcb80694ec09cfbec31cc
Saqib Shafiq <saqibshafiq618@gmail.com>
2026-06-18 11:13:43 +0500

    Time: 0 ms (100.00%) | Memory: 20.6 MB (93.36%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -4,35 +4,55 @@ public:
     int n = matrix.size();
     int m = matrix[0].size();
 
-    // Mark original zeros
+
+    bool flag_r = false, flag_c = false;
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+        if (matrix[i][0] == 0) {
+            flag_r = true;
+            break;
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        if (matrix[0][i] == 0) {
+            flag_c = true;
+            break;
+        }
+    }
+
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < m; j++) {
             if (matrix[i][j] == 0) {
-                matrix[i][j] = INT_MAX-5;
+                matrix[i][0] = 0; //row
+                matrix[0][j] = 0; //column
             }
         }
     }
 
-    // Set rows and columns to zero
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (matrix[i][j] == INT_MAX-5) {
+    for (int i = 1; i < n; i++) {
+        for (int j = 1; j < m; j++) {
+            if (matrix[i][0] == 0 || matrix[0][j] == 0) {
                 matrix[i][j] = 0;
-
-                // Column j
-                for (int k = 0; k < n; k++) {
-                    if (matrix[k][j] != INT_MAX-5)
-                        matrix[k][j] = 0;
-                }
-
-                // Row i
-                for (int k = 0; k < m; k++) {
-                    if (matrix[i][k] != INT_MAX-5)
-                        matrix[i][k] = 0;
-                }
             }
         }
     }
-}
+
+
+    
+
+    if (flag_c) {
+        for (int j = 0; j < m; j++) {
+            matrix[0][j] = 0;
+        }
+    }
+
+    if (flag_r ) {
+        for (int i = 0; i < n; i++) {
+            matrix[i][0] = 0;
+        }
+    }
+
+
+   }
 
 };
